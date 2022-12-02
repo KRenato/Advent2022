8ed8534e481e626562674e3e3f5559c102db24d9
Kevin Renato <kev.renato@gmail.com>
2022-12-01 21:52:26 -0500

    Initial commit.

---
+++ Common/InputReader.cs
@@ -0,0 +1,11 @@
+﻿namespace Common;
+
+public static class InputReader
+{
+    private const string fileName = "input.txt";
+
+    public static async Task<string[]> GetInputAsync()
+    {
+        return await File.ReadAllLinesAsync(fileName);
+    }
+}
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Common;
+
+var input = await InputReader.GetInputAsync();
+
+Console.WriteLine("Part 1" + Environment.NewLine);
+
+Part1(input);
+
+Console.WriteLine(Environment.NewLine + "Part 2" + Environment.NewLine);
+
+Part2(input);
+
+static void Part1(string[] input)
+{
+    var elves = new List<int>();
+    var total = 0;
+
+    foreach (var item in input)
+    {
+        if (int.TryParse(item, out var value))
+        {
+            total += value;
+            continue;
+        }
+
+        elves.Add(total);
+        total = 0;
+    }
+
+    Console.WriteLine("The elf with the most food has: {0}", elves.Max());
+}
+
+static void Part2(string[] input)
+{
+    var elves = new List<int>();
+    var total = 0;
+
+    foreach (var item in input)
+    {
+        if (int.TryParse(item, out var value))
+        {
+            total += value;
+            continue;
+        }
+
+        elves.Add(total);
+        total = 0;
+    }
+
+    var top3 = elves.OrderByDescending(i => i)
+        .Take(3)
+        .Sum(i => i);
+
+    Console.WriteLine("The elf with the most food has: {0}", top3);
+}
